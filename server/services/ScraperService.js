f58cbff2ef710af2daed3230ce68a7598fa24a98
KorbinWilliams <52514607+KorbinWilliams@users.noreply.github.com>
2020-05-07 13:40:12 -0600

    need to figure out server issue with postman

--- server/services/ScraperService.js
+++ server/services/ScraperService.js
@@ -5,8 +5,14 @@ import puppeteer from "puppeteer"
 // const _repository = mongoose.model('Scraper', Scraper)
 
 class ScraperService {
+  // TODO Need to decide how complicated this will be. First make the static querys. Then possibly expand to things like url specific querys/filters.
+  //NOTE need to check query-selectors. Might be differences in screen size(is puppeteer screen size constant?)
+  //TODO AMAZON SPECIFIC for-loop over array of data-indexes. create an object for each title/img/price 
+  //NOTE AMAZON TOP LEVEL PARENT =<div data-index="${num}">
+  // Title: <span class="a-size-medium a-color-base a-text-normal" dir="auto">
+  //Image: PARENT = <div class="a-section aok-relative s-image-fixed-height"> CHILD = <img class="s-image">
+  // Price: CHILD = <span class="a-price-whole"> 
   async getAll(url) {
-    // TODO Need to decide how complicated this will be. First make the static querys. Then possibly expand to things like url specific querys/filters.
     let urlArr = url.split('.')
     let website = urlArr[2]
 
@@ -15,22 +21,18 @@ class ScraperService {
         const page = await browser.newPage()
         await page.goto(url)
         await page.screenshot({ path: url })
+
+        if (website == 'amazon') {
+          let productImage = await page.evaluate(() => document.querySelector("div img.s-image").textContent)
+          console.log(productImage)
+        }
+        else if (website == 'ebay') {
+
+        }
+        else {
+
+        }
       })
-    //NOTE need to check query-selectors. Might be differences in screen size(is puppeteer screen size constant?)
-    if (website == 'amazon') {
-      //TODO for-loop over array of data-indexes. create an object for each title/img/price 
-      //NOTE TOP LEVEL PARENT =<div data-index="${num}">
-      // Title: <span class="a-size-medium a-color-base a-text-normal" dir="auto">
-      //Image: PARENT = <div class="a-section aok-relative s-image-fixed-height"> CHILD = <img class="s-image">
-      // Price: CHILD = <span class="a-price-whole"> 
-
-    }
-    else if (website == 'ebay') {
-
-    }
-    else {
-
-    }
   }
 }
 
