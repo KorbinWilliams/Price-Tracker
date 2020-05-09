4b491fea6e42aefef4968bf529ca8af7c7db054c
KorbinWilliams <52514607+KorbinWilliams@users.noreply.github.com>
2020-05-09 15:27:47 -0600

    figured out how to access parent via data-index

--- server/services/ScraperService.js
+++ server/services/ScraperService.js
@@ -7,11 +7,15 @@ import puppeteer from "puppeteer";
 class ScraperService {
   // TODO Need to decide how complicated this will be. First make the static querys. Then possibly expand to things like url specific querys/filters.
   //NOTE need to check query-selectors. Might be differences in screen size(is puppeteer screen size constant?)
-  //TODO AMAZON SPECIFIC for-loop over array of data-indexes. create an object for each title/img/price
-  //NOTE AMAZON TOP LEVEL PARENT =<div data-index="${num}">
-  // Title: <span class="a-size-medium a-color-base a-text-normal" dir="auto">
-  //Image: PARENT = <div class="a-section aok-relative s-image-fixed-height"> CHILD = <img class="s-image">
-  // Price: CHILD = <span class="a-price-whole">
+  checkProductArray(propToEval, queryStr, parentQuery) {
+    // count = 0
+    // document.find(parent => parentDataIndex = count)
+    let propVal = parentQuery.querySelector(queryStr);
+    if (propVal.textContent != null) {
+      propToEval = propVal.textContent;
+    }
+  }
+
   async getAll(url) {
     let urlArr = url.split(".");
     let website = urlArr[1];
@@ -19,41 +23,26 @@ class ScraperService {
     const page = await browser.newPage();
     await page.goto(url);
 
-    // input[name='login']
     if (website == "amazon") {
-      //NOTE returns the first 10 titles of an amazon search
-      // let productTitles = await page.evaluate(() =>
-      //   Array.from(document.querySelectorAll("h2 span"))
-      //     .map((partner) => partner.textContent)
-      //     .slice(0, 10)
-      // );
-      // let productImages = await page.evaluate(() =>
-      //   Array.from(document.querySelectorAll("img"))
-      //     .map((partner) => partner.src)
-      //     .slice(0, 10)
-      // );
-      let productPrices = page.evaluate(() =>
-        Array.from(document.querySelectorAll("span.a-price-whole"))
-          .map((partner) => partner.textContent)
-          .slice(0, 10)
-      );
-
+      //NOTE returns the first 10 products of an amazon search
       let productArray = page.evaluate(() =>
         Array.from(document.querySelectorAll("div.s-include-content-margin"))
-          .map((compact) => ({
-            title: compact.querySelector("h2 span").textContent,
-            image: compact.querySelector("img").src,
-            price: compact.querySelector("span.a-price-whole").textContent,
+          .map((product) => ({
+            title: product.querySelector("h2 span").textContent,
+            image: product.querySelector("img").src,
+            //NOTE need to take off price evaluate separately
+            price: "",
           }))
           .slice(0, 10)
       );
-      return productArray;
-      // else if (website == 'ebay') {
+      // this.checkProductArray(document.querySelectorAll("div.s-include-content-margin"), )
 
-      // }
-      // else {
+      let productCheck = page.evaluate(
+        () => document.querySelector("div[data-index='0'] h2 span").innerHTML
+      );
 
-      // }
+      return productCheck;
+      // return productArray;
     }
   }
 }
