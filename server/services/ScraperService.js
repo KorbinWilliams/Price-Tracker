4b3c0c2abc9acdb51a86f1a92667651838530101
KorbinWilliams <52514607+KorbinWilliams@users.noreply.github.com>
2020-05-10 14:22:18 -0600

    making progress check out promise rejections

--- server/services/ScraperService.js
+++ server/services/ScraperService.js
@@ -7,16 +7,9 @@ import puppeteer from "puppeteer";
 class ScraperService {
   // TODO Need to decide how complicated this will be. First make the static querys. Then possibly expand to things like url specific querys/filters.
   //NOTE need to check query-selectors. Might be differences in screen size(is puppeteer screen size constant?)
-  checkProductArray(propToEval, queryStr, parentQuery) {
-    // count = 0
-    // document.find(parent => parentDataIndex = count)
-    let propVal = parentQuery.querySelector(queryStr);
-    if (propVal.textContent != null) {
-      propToEval = propVal.textContent;
-    }
-  }
 
   async getAll(url) {
+    //NOTE Takes in url opens puppeteer(chromium), then goes to url
     let urlArr = url.split(".");
     let website = urlArr[1];
     const browser = await puppeteer.launch();
@@ -25,7 +18,7 @@ class ScraperService {
 
     if (website == "amazon") {
       //NOTE returns the first 10 products of an amazon search
-      let productArray = page.evaluate(() =>
+      let productArray = await page.evaluate(() =>
         Array.from(document.querySelectorAll("div.s-include-content-margin"))
           .map((product) => ({
             title: product.querySelector("h2 span").textContent,
@@ -35,17 +28,29 @@ class ScraperService {
           }))
           .slice(0, 10)
       );
-      // this.checkProductArray(document.querySelectorAll("div.s-include-content-margin"), )
 
-      let productCheck = page.evaluate(
-        () => document.querySelector("div[data-index='0'] h2 span").innerHTML
-      );
+      let priceArray = [];
+      let count = 0;
 
-      return productCheck;
-      // return productArray;
+      for (let i = 0; count > 10; i++) {
+        const product = i;
+        await page.evaluate(() => {
+          let parent = document.querySelector(`div[data-index='${count}'] `);
+          let child = parent.getElementsByClassName(
+            "a-row a-size-base a-color-base"
+          );
+          console.log(child);
+          priceArray.push(child);
+        });
+      }
+
+      await browser.close();
+      return [productArray, priceArray];
     }
   }
 }
 
 const _scraperService = new ScraperService();
 export default _scraperService;
+
+// <div> a-row a-size-base a-color-base
