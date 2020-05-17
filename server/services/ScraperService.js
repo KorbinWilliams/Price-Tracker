7be84a76e66d3e8db87f65bfdcf6d45a905b69c7
KorbinWilliams <52514607+KorbinWilliams@users.noreply.github.com>
2020-05-17 14:35:13 -0600

    works sometimes got price

--- server/services/ScraperService.js
+++ server/services/ScraperService.js
@@ -16,36 +16,52 @@ class ScraperService {
   //   // other actions...
   //   await browser.close();
   // })();
+  async getProductArray(url) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    let productArray = await page.evaluate(() =>
+      Array.from(document.querySelectorAll("div.s-include-content-margin"))
+        .map((product) => ({
+          title: product.querySelector("h2 span").textContent,
+          image: product.querySelector("img").src,
+          //NOTE need to take off price evaluate separately
+          price: product.querySelector("div.a-size-base").innerHTML || 0,
+        }))
+        .slice(0, 10)
+    );
+    return productArray;
+  }
+  replacePrice(productArray) {
+    for (let i = 0; i < productArray.length; i++) {
+      const product = productArray[i];
+      if (product.price != 0) {
+        if (product.price.indexOf("a-offscreen") != -1) {
+          let priceIndex = product.price.indexOf("a-offscreen");
+          let cutout = product.price.substring(
+            priceIndex,
+            product.price.length
+          );
+          product.price = cutout.substring(13, cutout.indexOf("<"));
+        }
+      }
+    }
+    return productArray;
+  }
 
   async getAll(url) {
     //NOTE Takes in url opens puppeteer(chromium), then goes to url
     let urlArr = url.split(".");
     let website = urlArr[1];
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
+    // const browser = await puppeteer.launch();
+    // const page = await browser.newPage();
+    // await page.goto(url);
 
     if (website == "amazon") {
       //NOTE returns the first 10 products of an amazon search
-      let productArray = await page.evaluate(() =>
-        Array.from(document.querySelectorAll("div.s-include-content-margin"))
-          .map((product) => ({
-            title: product.querySelector("h2 span").textContent,
-            image: product.querySelector("img").src,
-            //NOTE need to take off price evaluate separately
-            price: product.querySelector("div.a-size-base").innerHTML || 0,
-          }))
-          .slice(0, 10)
-      );
-
-      for (let i = 0; i < productArray.length; i++) {
-        const product = productArray[i];
-        if (product.price != 0) {
-          // @ts-ignore
-          product.price = product.price.indexOf("$");
-        }
-      }
-      await browser.close();
+      let productArray = await this.getProductArray(url);
+      productArray = this.replacePrice(productArray);
+      // await browser.close();
       return productArray;
     }
   }
